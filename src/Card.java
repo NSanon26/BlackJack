c067674ebb602fb529446f1f4d6e44a349a6f1d7
NSanon26 <nehemiah_sanon26@milton.edu>
2024-05-17 10:14:36 -0400

    I made a card class and tried to make it work

--- src/Card.java
+++ src/Card.java
@@ -5,8 +5,22 @@ public class Card {
     // value 1-11
     public int value;
 
+    //how much each card is worth
+    public int points;
+
     //Name like ace,2 ,3 etc
     public String name;
+
+    public static void main(String[] args) {
+        Card x = new Card();
+    }
+
+    public Card(){
+        //suit = I don't know what to write here
+        //value = math.random()*10 maybe
+        //if(value = 2){points = 2} maybe
+        System.out.print("the" + value+ " of" + suit+ "is worth" + points+ "points");
+    }
 
 
 }
