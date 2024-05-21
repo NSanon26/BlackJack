2730fb044bde1b0546385fc88d42e66341288e8d
NSanon26 <nehemiah_sanon26@milton.edu>
2024-05-21 10:36:16 -0400

    I have all 52 cards

--- src/Card.java
+++ src/Card.java
@@ -5,22 +5,85 @@ public class Card {
     // value 1-11
     public int value;
 
-    //how much each card is worth
-    public int points;
+
 
     //Name like ace,2 ,3 etc
     public String name;
 
-    public static void main(String[] args) {
-        Card x = new Card();
-    }
-
-    public Card(){
-        //suit = I don't know what to write here
-        //value = math.random()*10 maybe
-        //if(value = 2){points = 2} maybe
-        System.out.print("the" + value+ " of" + suit+ "is worth" + points+ "points");
-    }
 
 
+    public Card(int pName, int pSuit){
+      // name = pName;
+        //suit = pSuit;
+        if(pName == 0){
+            value = 11;
+            name = "Ace";
+        }
+        if(pName == 1){
+            value = 2;
+            name = "Two";
+        }
+        if(pName == 2){
+            value = 3;
+            name = "Three";
+        }
+        if(pName == 3){
+            value = 4;
+            name = "Four";
+        }
+        if(pName == 4){
+            value = 5;
+            name = "Five";
+        }
+        if(pName == 5){
+            value = 6;
+            name = "Six";
+        }
+        if(pName == 6){
+            value = 7;
+            name = "Seven";
+        }
+        if(pName == 7){
+            value = 8;
+            name = "Eight";
+        }
+        if(pName == 8){
+            value = 9;
+            name = "Nine";
+        }
+        if(pName == 9){
+            value = 10;
+            name = "Ten";
+        }
+        if(pName == 10){
+            value = 10;
+            name = "Jack";
+        }
+        if(pName == 11){
+            value = 10;
+            name = "Queen";
+        }
+        if(pName == 12){
+            value = 10;
+            name = "King";
+        }
+        if(pSuit ==0){
+            suit = "Spades";
+        }
+        if(pSuit ==1){
+            suit = "Clubs";
+        }
+        if(pSuit ==2){
+            suit = "Hearts";
+        }
+        if(pSuit ==3){
+            suit = "Diamonds";
+        }
+
+
+
+}
+public void print(){
+    System.out.println("The " + name+ " of " + suit+ " is worth " + value+ " points");
+}
 }
--- src/Casino.java
+++ src/Casino.java
@@ -1,9 +1,31 @@
 public class Casino {
+    public Card cards;
+    public Card[] deck;
     public static void main(String[] args) {
         Casino x = new Casino();
 
     }
 public Casino(){
         System.out.println("Welcome to Nemo's Casino.");
+
+        deck = new Card[52];
+
+int door = 0;
+for(int j = 0; j<4; j++) {
+    for (int i = 0; i < 13; i++) {
+        cards = new Card(i, j);
+        cards.print();
+        deck[door]=cards;
+        door ++;
+    }
 }
+printDeck();
+    }
+    public void printDeck(){
+        for(int i =0; i<deck.length; i++){
+            //System.out.println(deck[i]);
+            deck[i].print();
+        }
+    }
 }
+
